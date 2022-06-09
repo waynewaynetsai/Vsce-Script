ea19d092ee40b049d749ee98d5421c8acc933cef
waynewaynetsai <wayne040904007@gmail.com>
2022-06-10 00:28:25 +0800

    refactor(handler.ts): update import path

--- src/registry/handler.ts
+++ src/registry/handler.ts
@@ -6,7 +6,7 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
 import { QuickpickCommandItem, QuickpickSetting } from "../models";
-import { Instantiator } from "../instantiator";
+import { Instantiator } from "../instance";
 import { CommandRegistry } from "./registry";
 import { CommandTable } from "./table";
 import { Library } from "../library";
